a9953207cc883414b7461692cdc43b36732b9a6a
Emily Bach [For Work] <emilyjbach@gmail.com>
2025-12-15 20:06:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -588,8 +588,11 @@ try:
             unsafe_allow_html=True,
     )
     st.caption(
-        "GR 237 is a monthly report prepared by the California Department of Social Services that tracks key metrics associated with California's General Relief and Interim Assistance programs, California's cash assistance programs." 
+        "GR 237 is a monthly report prepared by the California Department of Social Services that tracks key metrics associated with California's General Relief and Interim Assistance programs. These programs administer cash aid to thousands of Californians each month." 
     )
+   st.caption(
+    "There are no strict rules for interpreting this data and local sources with a direct tie to impacted communities should be consulted for a full explanation of trends. The Roots Community Health Center, for example, prepared an **[excellent report](https://rootscommunityhealth.org/wp-content/uploads/2014/07/GA_eval_12.pdf)** utilizing and contextualizing data trends in Alameda County's GA Program."
+)
 
 except Exception as e:
     st.error("The app crashed. Here’s the full error (so it won’t look like a blank page):")
